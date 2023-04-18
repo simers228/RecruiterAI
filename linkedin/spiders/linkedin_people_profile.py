8107f76870c4c51d9fa768019a56e502e1af99f2
Simer <simers228@gmail.com>
2023-04-18 10:53:37 -0500

    sampson profile model

--- linkedin/spiders/linkedin_people_profile.py
+++ linkedin/spiders/linkedin_people_profile.py
@@ -1,4 +1,7 @@
 import scrapy
+import logging
+import ast
+
 
 class LinkedInPeopleProfileSpider(scrapy.Spider):
     name = "linkedin_people_profile"
@@ -8,23 +11,11 @@ class LinkedInPeopleProfileSpider(scrapy.Spider):
         }
 
     def start_requests(self):
-        profile_list = ['catherine-ainikkal-146a0510a','rachel-marshall-montavon-68604526', 'nmarion1', 'adriana-campa-134153149','ray-beckman-p-e-067652a2',
-                        'joe-nikolai-084110114', 'agratha-reddy-8a6048113','eszter-csicsai-94a2096b', 'ali-noel-406447218','justin-johnson-pe-cxa-598b0a91',
-                         'angelaberes', 'nicholas-knoke-gsp-792b93130', 'joshua-koss-gsp-7615291a2', 'ryan-clinton-145ab018a','mike-baccus-cet-30a772141', 
-                         'paul-wick', 'andrew-roensch-43b866a2', 'keith-mcnamee-60031129', 'brian-filiatrault-1638a391', 'paul-petit-079282b6', 
-                         'alfred-ladores-88873198', 'tayler-budenski-matzke-05404080','danielle-breva-81071732', 'danae-ticey-moore-9637a834', 'jordanxhuang', 
-                         'allison-oyos-b76686102','meg-anderson-51b0698', 'paul-noll-8832518a', 'charles-lentz-bb908727', 'tim-kittila-p-e-5b36888', 
-                         'david-santos-15a484114','dan-blossey-68383938', 'jimmy-garcia-3b947a192', 'alexander-vera-b8762284', 'weston-kolles-57a568130',
-                         'taylor-boileau', 'michael-dahdal-771b53181', 'alberto-martinez28', 'kjmaki', 'jesse-peralta-0496971b1',
-                         'paulkieffer', 'ian-dolph-6b8060128', 'todd-hensley-49887b77', 'dean-rossi-2824b985', 'kirk-blasdel-1b2b2581',
-                         'patrick-sims-b7b59183', 'jeff-martin-a88a8a30', 'lucas-hubbard-742110167', 'randy-berner-b831367', 'michael-ortiz-b2806232',
-                         'john-huyett-pe', 'caleb-frost', 'chrisannzeller', 'lindsey-van-vuuren', 'carol-spalony-34408a4a',
-                         'aaric-witt-2a279412a', 'pcdodds', 'steven-jackson-ab029679', 'ben-r-bowman', 'carlakolber',
-                         'melissa-montiel-a21181176', 'terry-stewart-csp-csm-b-a-s-etc-15306616b', 'keith-burch-983264122', 'bob-millerbernd-a9335a241', 'andy-brown-861ba097',
-                         'jose-chavez-8146821a1', 'josh-helgesen-605b5195', 'jacob-machone-pe-19a04661', 'chrisbristowgreiner', 'aleisha-kiley-46ba7462',
-                         'josh-miltenberger-033799a', 'spencer-greiner-86463554', 'bryce-williamson-6483262a', 'adam-allison-dpt-coss-75b40914a', 'yalda-akbarpour',
-                         'mark-close', 'jessica-venegas-9bb01968', 'tim-benbow-92a34698', 'anthonykriens', 'asha-garcia-87b239b3'
-                         ]
+        logging.basicConfig(filename='linkedin_people_profile.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+        with open('company_profiles/sampson_construction.txt', 'r') as file:
+            content = file.read().strip()
+            profile_list = ast.literal_eval(content)
         for profile in profile_list:
             linkedin_people_url = f'https://www.linkedin.com/in/{profile}/' 
             yield scrapy.Request(url=linkedin_people_url, callback=self.parse_profile, meta={'profile': profile, 'linkedin_url': linkedin_people_url})
@@ -33,6 +24,7 @@ class LinkedInPeopleProfileSpider(scrapy.Spider):
         item = {}
         item['profile'] = response.meta['profile']
         item['url'] = response.meta['linkedin_url']
+        logging.info(f"Processing profile: {item['profile']}")
 
         """
             SUMMARY SECTION
@@ -48,6 +40,7 @@ class LinkedInPeopleProfileSpider(scrapy.Spider):
             item['location'] = summary_box.css('span.top-card__subline-item::text').get().strip()
             if 'followers' in item['location'] or 'connections' in item['location']:
                 item['location'] = ''
+                logging.warning(f"Invalid location for profile {item['profile']}")
 
         item['followers'] = ''
         item['connections'] = ''
@@ -76,6 +69,7 @@ class LinkedInPeopleProfileSpider(scrapy.Spider):
             try:
                 experience['organisation_profile'] = block.css('h4 a::attr(href)').get().split('?')[0]
             except Exception as e:
+                logging.error(f"experience --> organisation_profile error for {item['profile']}: {e}")
                 print('experience --> organisation_profile', e)
                 experience['organisation_profile'] = ''
                 
@@ -131,6 +125,7 @@ class LinkedInPeopleProfileSpider(scrapy.Spider):
             try:
                 education['organisation'] = block.css('h3::text').get().strip()
             except Exception as e:
+                logging.error(f"education --> organisation error for {item['profile']}: {e}")
                 print("education --> organisation", e)
                 education['organisation'] = ''
 
