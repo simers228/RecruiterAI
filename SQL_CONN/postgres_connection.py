1a3294d0ca537e1bae9eb929792a636e30f70b0b
aidang1003 <60905689+aidang1003@users.noreply.github.com>
2023-04-26 21:15:43 -0600

    Postgres connection v2

--- SQL_CONN/postgres_connection.py
+++ SQL_CONN/postgres_connection.py
@@ -1,4 +1,7 @@
+import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy import text
+import psycopg2
 
 host = '10.0.0.218'  # IP on my local network - watch for dynamic IP after a machine restart
 port = '5432'  # Postgres is on port 5432
@@ -6,10 +9,36 @@ db_name = 'SQMAIN_DB'  # Replace with the name of your PostgreSQL database
 user = 'usr_sqlalchemy'  # Replace with the username you use to connect to your PostgreSQL database
 password = 'testconnection123'  # Replace with the password you use to connect to your PostgreSQL database
 
-engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db_name}')
+connectionString = f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
+print(connectionString)
+engine = create_engine(connectionString)
+
+# Open connection
+connection = engine.connect()
+print("Connection great success!")
+
+# Verify we are connected to the correct database
+qry0 = text("SELECT current_database();")
+result0 = connection.execute(qry0)
+for row in result0:
+    print(row)
+
+# Create a SQL statement object
+qry1 = text("SELECT * from tbl_LinkedinExperience")
+#qry1 = text("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema';")
 
 
-with engine.connect() as conn:
-    result = conn.execute('SELECT * FROM tbl_PeopleProfile')
-    for row in result:
-        print(row)
+# Execute the statement
+result = connection.execute(qry1)
+
+# Print the column names
+print(result.keys())
+
+# Print the query results
+for row in result:
+    print(row)
+
+# Close connection
+connection.close()
+
+
